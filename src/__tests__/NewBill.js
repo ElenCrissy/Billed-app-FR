e750b9b6a36f7ed3861eff4b457702d2698263c0
Elencrissy <cristina.furgala@gmail.com>
2021-09-11 16:01:49 +0200

    New Bill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import { getByTestId } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
@@ -7,15 +7,31 @@ import { htmlPrefilter } from "jquery"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then only jpg, jpeg and png files should be accepted", async () => {
+    test("Then only jpg, jpeg and png files should be accepted", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      const file = await screen.queryByTestId("file")
+      // Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      // window.localStorage.setItem('user', JSON.stringify({
+      //   type: 'Employee'
+      // }))
+      // const onNavigate = (pathname) => {
+      //   document.body.innerHTML = ROUTES({ pathname })
+      // }
+      // const newBill = new NewBill({
+      //   document, onNavigate, firestore: null, localStorage: window.localStorage
+      // })          
+      const file = screen.queryByTestId("file")
+      // const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e)) 
+      // file.addEventListener('input', handleChangeFile)
+      // userEvent.change(file)
+
+      fireEvent.change(file, {target : File.name})
+
       const fileName = file.value
       const fileExtension = fileName.split('.').pop()
       const extensions = ".jpg" || ".jpeg" || ".png"
       //to-do write assertion
-      expect(fileExtension).toBe(extensions)
+      expect(fileExtension).toEqual(extensions)
     })
   })
 })
