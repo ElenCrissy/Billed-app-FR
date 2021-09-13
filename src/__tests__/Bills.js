b26f024d2954f9f9dab42f0c7ab72b77c9a29c1b
Elencrissy <cristina.furgala@gmail.com>
2021-09-13 14:14:30 +0200

    Unit test bill

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,42 +2,43 @@ import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 
+const mock = (function() {
+  const store = {};
+  return {
+    getItem: function(key) {
+      return store[key];
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    clear: function() {
+      store = {};
+    }
+  };
+})();
+
+// Object.defineProperty(window, "localStorage", {
+    //   value: {
+    //     getItem: jest.fn(() => null),
+    //     setItem: jest.fn(() => null)
+    //   },
+    //   writable: true
+    // });
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    // var mock = (function() {
-    //   var store = {};
-    //   return {
-    //     getItem: function(key) {
-    //       return store[key];
-    //     },
-    //     setItem: function(key, value) {
-    //       store[key] = value.toString();
-    //     },
-    //     clear: function() {
-    //       store = {};
-    //     }
-    //   };
-    // })();
-    // Object.defineProperty(window, 'localStorage', { value: mock });
-
-    Object.defineProperty(window, "localStorage", {
-      value: {
-        getItem: jest.fn(() => null),
-        setItem: jest.fn(() => null)
-      },
-      writable: true
-    });
-    
+    Object.defineProperty(window, 'localStorage', { value: mock });
     // const user = localStorage.getItem('user')
     //if user = employee then test :
-    // jest.spyOn(Storage.prototype, 'setItem')
+    jest.spyOn(Storage.prototype, 'setItem')
+
     test("Then bill icon in vertical layout should be highlighted", () => {
       const html = BillsUI({ data: []})
       document.body.innerHTML = html
       //to-do write expect expression
-      // set page on employee mode
       // jest.spyOn(window.localStorage.__proto__, 'getItem')
-      // window.localStorage.__proto__.getItem = jest.fn();
+      // set page on employee mode
+      expect(window.localStorage.getItem('user')).toBe('Employee');
       // select icon-window testId and check if highlighted
       const iconWindow = screen.getByTestId('icon-window')
       expect(iconWindow).toHaveClass('.active-icon')
