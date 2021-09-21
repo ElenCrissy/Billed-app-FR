bbae60ea9f248f57f584dfbb8a6ed5155fe53eb4
Elencrissy <cristina.furgala@gmail.com>
2021-09-21 17:51:11 +0200

    NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,10 +1,10 @@
-import { fireEvent, screen } from "@testing-library/dom"
+import { screen } from "@testing-library/dom"
 import { getByTestId } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import { htmlPrefilter } from "jquery"
-// import imageJPG from '../assets/images/facturefreemobile.jpg';
-// import imagePNG from '../assets/images/algo1v3.png';
+import userEvent from "@testing-library/user-event"
+import '@testing-library/jest-dom'
 
 
 
@@ -14,10 +14,10 @@ describe("Given I am connected as an employee", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
 
-      const file = screen.getByTestId("file")
-      file.value = ''
+      const input = screen.getByTestId("file")
+      input.value = ''
 
-      expect(file).toBeTruthy()
+      expect(input).toBeInTheDocument()
 
       const firestoreMock = {
         storage : {
@@ -30,41 +30,18 @@ describe("Given I am connected as an employee", () => {
         }
       }
 
-      const newBill = new NewBill({document, firestore : firestoreMock})
-      const imgJPG = document.createElement('img')
-      // imgJpg.src = '../assets/images/facturefreemobile.jpg'
-      // imgJpg.type = 'image/jpg'
+      const filePNG = new File(['hello'], 'hello.png', {type: 'image/png'})
+      const filePDF = new File(['hello'], 'hello.pdf', {type: 'application/pdf'})
 
-      // const imgJPG = require('../assets/images/facturefreemobile.jpg')
-      // const imgPNG = require('../assets/images/algo1v3.png')
-      
-      const imgJPGMock = jest.mock(imgJPG, () => {name: 'facturefreemobile.jpg'})
-      // const imgPNGMock = jest.mock(imgPNG, () => 'algo1v3.png')
-
-      // fireEvent.change(file, {target : {value : `${imgJPGMock}`}})
-      // expect(file.name).toEqual(imgJPGMock.name)
-
-      const fileDetails = file.files[0]
-      const fileName = file.value 
-      const fileExtension = fileName.split('.').pop()
-      fileExtension = ('.jpg' || '.png')
-
-      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e)) 
-      file.addEventListener('input', handleChangeFile)
-      expect(handleChangeFile()).toHaveBeenCalled()
+      const newBill = new NewBill({document, firestore : firestoreMock})  
 
       //put appelé avec jpg et png
-      expect(firestoreMock.storage.put(fileDetails)).toHaveBeenCalled()
+      userEvent.upload(input, filePNG)
+      expect(firestoreMock.storage.ref).toHaveBeenCalled()
 
       //put non appelé avec autre
-      fileExtension = !('.jpeg' || '.jpg' || '.png')
-      expect(firestoreMock.storage.put(fileDetails)).not.toHaveBeenCalled()
-
-      // const fileName = file.value
-      // const fileExtension = fileName.split('.').pop()
-      // const extensions = [".jpg", ".jpeg", ".png"]
-      // //to-do write assertion
-      // expect(extensions).toContain(fileExtension)
+      userEvent.upload(input, filePDF)
+      expect(firestoreMock.storage.put).not.toHaveBeenCalled()
     })
   })
 })
