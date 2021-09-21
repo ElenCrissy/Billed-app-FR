3f5674837f398aec2ebb551b6c03fc98be27a56f
Elencrissy <cristina.furgala@gmail.com>
2021-09-21 17:01:57 +0200

    Test unitaire bill

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen, getByTestId } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import Bills from "../containers/Bills.js"
@@ -20,7 +20,7 @@ describe("Given I am connected as an employee", () => {
       const userString = window.localStorage.getItem('user')
       const user = JSON.parse(userString)
       // expect(window.location.hash).toBe("#employee/bill/")
-      expect("#employee/bill/").toBe(window.location.hash)
+      expect(window.location.hash).toBe("#employee/bill/")
       expect(user.type).toBe('Employee')
 
       // selectionner icon-window testId et vérifier si highlighted
@@ -39,43 +39,86 @@ describe("Given I am connected as an employee", () => {
   })
 })
 
+// test unitaire
+
+describe("Given I am user connected as Employee", () => {
+  describe("When I navigate to the bills", async () => {
+    const getSpy = jest.spyOn(firebase, "get")
+    const bills = await firebase.get()
+
+    test.each(bills)("bill should have info and status", () => {
+      expect(getSpy).toHaveBeenCalled()
+
+      const billType = screen.getByTestId("bill-type")
+      const billAmount = screen.getByTestId("bill-amount")
+      const billDate = screen.getByTestId("bill-date")
+      const billStatus = screen.getByTestId("bill-status")
+      const iconEye = screen.getByTestId("icon-eye")
+      expect(iconEye).toBeTruthy()
+      expect(billType).toEqual(bill.type)
+      expect(billAmount).toEqual(bill.amount)
+      expect(billDate).toHaveText(bill.date)
+      expect(billStatus).toEqual(bill.status)
+    })
+
+    describe("When I click on icon-eye", () => {
+
+      test.each(bills)("Then opens modal with image", () => {
+  
+        const firestoreMock = {
+          storage : {
+            ref : jest.fn().mockReturnThis(),
+            put : jest.fn().mockImplementation(() => Promise.resolve({
+              ref: {
+                getDownloadURL : jest.fn()
+              } 
+            }))
+          }
+        }
+  
+        // const mockCallBack = jest.fn();
+  
+        // const button = shallow((<Button onClick={mockCallBack}>Ok!</Button>));
+        // button.find('button').simulate('click');
+        // expect(mockCallBack.mock.calls.length).toEqual(1);
+  
+        const bill = new Bills({document, firestore : firestoreMock})
+        const iconEye = screen.getByTestId("icon-eye")
+        const handleClickIconEye = jest.fn((e) => bill.handleClickIconEye(icon)) 
+        iconEye.addEventListener('click', handleClickIconEye)
+        expect(handleClickIconEye()).toHaveBeenCalled()
+      })
+    })
+  })
+})
+
 // test d'intégration GET
 
 describe("Given I am a user connected as Employee", () => {
   describe("When I navigate to the bills", async () => {
     const getSpy = jest.spyOn(firebase, "get")
     const bills = await firebase.get()
+    expect(getSpy).toHaveBeenCalledTimes(1)
+    expect(bills.data.length).toBe(4)
 
-    test.each(bills)("bill should have info and status", async () => {
-      // expect(getSpy).toHaveBeenCalledTimes(1)
-
-      const status = ["accepted", "pending", "refused"]
-      expect(status).toContain(bill.status)
-      expect(bill.name).not.toBeFalsy()
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 404"))
+      )
+      const html = BillsUI({ error: "Erreur 404" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
     })
 
-    // test("fetches bills from an API and fails with 404 message error", async () => {
-    //   firebase.get.mockImplementationOnce(() =>
-    //     Promise.reject(new Error("Erreur 404"))
-    //   )
-    //   const html = DashboardUI({ error: "Erreur 404" })
-    //   document.body.innerHTML = html
-    //   const message = await screen.getByText(/Erreur 404/)
-    //   expect(message).toBeTruthy()
-    // })
-
-    // test("fetches messages from an API and fails with 500 message error", async () => {
-    //   firebase.get.mockImplementationOnce(() =>
-    //     Promise.reject(new Error("Erreur 500"))
-    //   )
-    //   const html = DashboardUI({ error: "Erreur 500" })
-    //   document.body.innerHTML = html
-    //   const message = await screen.getByText(/Erreur 500/)
-    //   expect(message).toBeTruthy()
-    // })
-
-    // test("Then it should show image", () => {
-
-    // })
+    test("fetches messages from an API and fails with 500 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 500"))
+      )
+      const html = BillsUI({ error: "Erreur 500" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
   })
 })
